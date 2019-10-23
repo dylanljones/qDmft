2b9698be514e1d94b0530118054d89140ddd42c0
dylanljones <dylanljones94@gmail.com>
2019-10-23 09:43:26 +0200

    cleaning up

--- test.py
+++ test.py
@@ -13,34 +13,6 @@ from qsim.core.gates import X_GATE, cgate, rx_gate, single_gate, pauli
 from scitools import Plot
 
 
-def get_circuit(new=False, file="circuits/test.circ"):
-    if new or not os.path.isfile(file):
-        print(f"Saving circuit: {file}")
-        c = Circuit(2)
-        c.h(0)
-        c.cx(0, 1)
-        # c.m()
-        c.save("circuits/test")
-        return c
-    else:
-        print(f"Loading circuit: {file}")
-        return Circuit.load(file)
-
-
-def time_evolution_circuit(arg, step):
-    c = Circuit(5, 1)
-    c.h(0)
-    c.cx(0, 1)
-    for i in range(step):
-        c.xy(1, 2, arg)
-        c.xy(3, 4, arg)
-        c.b(1, 3, arg)
-    c.cx(0, 1)
-    c.h(0)
-    c.m(0, 0)
-    return c
-
-
 def main():
     v, t, n = 4, 6, 12
     arg = v/2 * t/n
