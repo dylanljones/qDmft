e073d212693c3adc745312f4d0e21624baee90fc
Dylan Jones <jonesdyl@fingolfin.physik.uni-augsburg.de>
2019-11-07 17:22:37 +0100

    measurments

--- test.py
+++ test.py
@@ -45,6 +45,53 @@ def measure_comp_basis(s, qubit):
     print(expectation(build_operator(0, Z_GATE, n), state))
 
 
+def project(statevector, op, qubit):
+    parts = [np.eye(2)] * statevector.n_qubits
+    parts[qubit] = op
+    proj = kron(parts)
+    return proj.dot(statevector.amp)
+
+
+def simulate_probability(p):
+    return int(np.random.random() <= p)
+
+
+def projective_measurement(s, qubit, shadow=False):
+    projection = project(s, P0, qubit)
+    p0 = np.dot(s.amp, projection)
+    value = simulate_probability(p0)
+    if value == 0:
+        state = projection
+    else:
+        state = project(s, P1, qubit)
+    if not shadow:
+        s.amp = state / la.norm(state)
+    return value
+
+
+
+def general_measurement(s, qubit, op=Z_GATE, shadow=False):
+    psi = s.amp
+
+    eigvals, eigvecs = la.eig(op)
+    v0, v1 = eigvecs.T
+
+    op = np.dot(v0[:, np.newaxis], v0[np.newaxis, :])
+    m = build_operator(qubit, op, s.n_qubits)
+    p0 = np.sum(np.abs(m.dot(psi))**2)
+    value = simulate_probability(p0)
+    if value == 0:
+        state = m.dot(psi)
+    else:
+        op = np.dot(v1[:, np.newaxis], v1[np.newaxis, :])
+        m = build_operator(qubit, op, s.n_qubits)
+        state = m.dot(psi)
+    if not shadow:
+        s.amp = state / la.norm(state)
+    return eigvals[value]
+
+
+
 def main():
     c = Circuit(2)
     c.h(0)
@@ -52,11 +99,15 @@ def main():
     # c.cx(0, 1)
     c.run()
 
-
     s = c.backend
     print(s)
-    measure_comp_basis(s, 0)
-
+    # print(projective_measurement(s, 0))
+    values = list()
+    for i in range(100):
+        x = general_measurement(s, 0, shadow=True).real
+        print(x)
+        values.append(x)
+    print("->", np.mean(values))
 
 
 
